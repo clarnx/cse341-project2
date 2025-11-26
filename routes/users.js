22c2abb0eda16b710e90c0787f8dc4f208b0e1c7
clarnx <clarenceonline@outlook.com>
2025-11-26 17:28:40 +0000

    added oAuth

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,39 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 const { validate, userValidationRules } = require("../middleware/validation");
+const isAuthenticated = require("../middleware/authenticate");
 
 router.get("/", userController.getAll);
 
-router.get("/:id", userValidationRules.getById, validate, userController.getSingle);
+router.get(
+  "/:id",
+  userValidationRules.getById,
+  validate,
+  userController.getSingle
+);
 
-router.post("/", userValidationRules.create, validate, userController.createUser);
+router.post(
+  "/",
+  isAuthenticated,
+  userValidationRules.create,
+  validate,
+  userController.createUser
+);
 
-router.put("/:id", userValidationRules.update, validate, userController.updateUser);
+router.put(
+  "/:id",
+  isAuthenticated,
+  userValidationRules.update,
+  validate,
+  userController.updateUser
+);
 
-router.delete("/:id", userValidationRules.delete, validate, userController.deleteUser);
+router.delete(
+  "/:id",
+  isAuthenticated,
+  userValidationRules.delete,
+  validate,
+  userController.deleteUser
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const mongodb = require("./data/database");
+const passport = require("passport");
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
+const cors = require("cors");
 
 const routes = require("./routes");
 const bodyParser = require("body-parser");
@@ -8,21 +12,73 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT DELETE, OPTIONS"
-  );
-  next();
+app
+  .use(bodyParser.json())
+  .use(
+    session({
+      secret: "secret",
+      resave: false,
+      saveUninitialized: true,
+    })
+  )
+  .use(passport.initialize())
+  .use(passport.session())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT DELETE, OPTIONS"
+    );
+    next();
+  })
+  .use(cors({ methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"] }))
+  .use(cors({ origin: "*" }))
+  .use("/", routes);
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
 });
 
-app.use("/", routes);
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.displayName}`
+      : "Logged Out"
+  );
+});
+
+app.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/api-doc",
+    session: false,
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect("/");
+  }
+);
 
 mongodb.initDb((err) => {
   if (err) {
