c9eade0554989b6a7286e9056a1eee0398577ab3
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2025-11-26 17:51:16 +0000

    Updated authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,6 +1,6 @@
 const isAuthenticated = (req, res, next) => {
   if (req.session.user === undefined) {
-    return res.status.json("You do not have access");
+    return res.status(401).json({ message: "You do not have access" });
   }
   next();
 };
